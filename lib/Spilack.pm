fff4f113586ebf0ca2fde0d9825c0c0160faf0ac
robertkrimen <robertkrimen@gmail.com>
2009-09-28 13:08:09 -0700

    Proper month parsing with start/stop inversion

--- lib/Spilack.pm
+++ lib/Spilack.pm
@@ -4,9 +4,22 @@ use strict;
 use warnings;
 
 use DateTimeX::Easy qw/datetime/;
+use Carp;
+
+my $tmp;
+
+sub parse_month {
+    my $class = shift;
+    my $input = shift;
+
+    my ($month, $year) = $input =~ m/^(\w{3})(\d{2})$/;
+    return unless $month = (datetime $month)->month;
+    
+    return DateTime->new( month => $month, year => 2000 + $year );
+}
 
 sub parse {
-    shift;
+    my $class = shift;
     my $input = shift;
 
     my @result;
@@ -22,6 +35,18 @@ sub parse {
         else {
             my ($range, $amount, $description) = split m/\s+/, $_, 3;
             my ($start, $stop) = $range =~ m/^(\w{3}\d{2})(?:\s*-\s*(\w{3}\d{2}))?$/;
+
+            croak "Unable to parse start $tmp" unless $start = $class->parse_month( $tmp = $start );
+            if ( $stop ) {
+                croak "Unable to parse stop $tmp" unless $stop = $class->parse_month( $tmp = $stop );
+                ( $start, $stop ) = ( $stop, $start ) if $start->epoch > $stop->epoch;
+                $stop = $stop->ymd;
+            }
+            $start = $start->ymd;
+
+#            croak "Unable to parse stop $tmp" if $stop && ! (($stop = datetime( $tmp = $stop )) && $stop = $stop->ymd);
+#            croak "Unable to parse start $tmp" unless (($start = datetime( $tmp = $start )) && $start = $start->ymd);
+#            croak "Unable to parse stop $tmp" if $stop && ! (($stop = datetime( $tmp = $stop )) && $stop = $stop->ymd);
             
             push @result, { start => $start, stop => $stop, amount => $amount, description => $description };
 
--- t/02-parse.t
+++ t/02-parse.t
@@ -14,8 +14,10 @@ my $input;
 $input = <<_END_;
 Jan01 1 Alice
 Jan01-Jun03 2 Bob not Alice
+Aug04-Jun03 3 Charlie
 _END_
 cmp_deeply( Spilack->parse( $input ), [
-    {qw/ start Jan01 amount 1/, description => 'Alice', stop => undef },
-    {qw/ start Jan01 stop Jun03 amount 2/, description => 'Bob not Alice', },
+    {qw/ start 2001-01-01 amount 1/, description => 'Alice', stop => undef },
+    {qw/ start 2001-01-01 stop 2003-06-01 amount 2/, description => 'Bob not Alice', },
+    {qw/ start 2003-06-01 stop 2004-08-01 amount 3/, description => 'Charlie', },
 ] );
